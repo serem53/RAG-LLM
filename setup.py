a983df7404e1c4dbd121709034bfea4934851ed0
trevor serem <trevorserem53@gmail.com>
2024-05-20 09:39:48 +0300

    data ingestion and model building

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="qasystem",
+    version="0.0.1",
+    author="trevor",
+    author_email="trevorerem53@gmail.com",
+    packages=find_packages(),
+    install_requires=["langchain","langchainhub","bs4","tiktoken","openai","boto3==1.34.37","langchain_community","chromadb","awscli",
+    "streamlit","pypdf","faiss-cpu"]
+)
+    
